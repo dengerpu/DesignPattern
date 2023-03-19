9680d0a7353ada3f39adaaec415fe3aa47a4de09
dep <1649743146@qq.com>
2023-03-19 16:48:36 +0800

    UML继承

---
+++ src/main/java/UML/dependency/IDCard.java
@@ -0,0 +1,9 @@
+package UML.dependency;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-19 16:41
+ */
+public class IDCard {
+}
---
+++ src/main/java/UML/dependency/PersonServiceBean.java
@@ -0,0 +1,21 @@
+package UML.dependency;
+
+import UML.Person;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-19 16:40
+ */
+public class PersonServiceBean {
+    private PersonDao personDao;//类
+
+    public void save(Person person){}
+
+    public IDCard getIDCard(Integer personid){
+        return null;
+    }
+    public void modify() {
+        Department department = new Department();
+    }
+}
