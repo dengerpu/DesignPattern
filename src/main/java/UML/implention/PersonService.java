a84c952a1e91da5b4c9cce479237725b77631c6b
dep <1649743146@qq.com>
2023-03-19 17:09:55 +0800

    UML实现

---
+++ src/main/java/UML/implention/PersonService.java
@@ -0,0 +1,10 @@
+package UML.implention;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-19 16:52
+ */
+public interface PersonService {
+    public void delete(int id);
+}
---
+++ src/main/java/UML/implention/PersonServiceImpl.java
@@ -0,0 +1,13 @@
+package UML.implention;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-19 16:53
+ */
+public class PersonServiceImpl implements PersonService{
+    @Override
+    public void delete(int id) {
+        System.out.println("删除");
+    }
+}
