ff4c1888cd0d376ced9a20abf984f74454c56412
dep <1649743146@qq.com>
2023-03-23 16:15:52 +0800

    建造者模式在JDK的应用-StringBuilder

---
+++ src/main/java/JDK_SRC/builder.java
@@ -0,0 +1,13 @@
+package JDK_SRC;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-23 15:42
+ */
+public class builder {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("123");
+        System.out.println(stringBuilder);
+    }
+}
