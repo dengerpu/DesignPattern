412aefd1fa706e37a6c5014e6f6512b9b49efe45
dep <1649743146@qq.com>
2023-03-24 19:49:24 +0800

    适配器模式--在springmvc中的应用

---
+++ src/main/java/Spring/springmvc/Test.java
@@ -0,0 +1,14 @@
+package Spring.springmvc;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-24 16:41
+ */
+public class Test {
+    public static void main(String[] args) {
+//        DispatcherServlet
+    }
+}
