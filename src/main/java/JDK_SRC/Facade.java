21bb704402885c7ae531a904bcc926eb3d5cac2d
dep <1649743146@qq.com>
2023-03-29 16:37:14 +0800

    外观模式的应用---Mybatis框架下的Configuration

---
+++ src/main/java/JDK_SRC/Facade.java
@@ -0,0 +1,15 @@
+package JDK_SRC;
+
+import org.apache.ibatis.session.Configuration;
+
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-27 22:29
+ */
+public class Facade {
+    public static void main(String[] args) {
+//        Configuration
+    }
+}
