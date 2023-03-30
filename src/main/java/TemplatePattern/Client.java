97dfeb8ca6d146d754f335ba2faca3054d6bc7bf
dep <1649743146@qq.com>
2023-03-30 15:29:50 +0800

    模板方法模式

---
+++ src/main/java/TemplatePattern/Client.java
@@ -0,0 +1,18 @@
+package TemplatePattern;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-30 15:15
+ */
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("-----制作红豆豆浆-------");
+        SoyaMilk redBeanSoyaMilk = new RedBeanSoyaMilk();
+        redBeanSoyaMilk.make();
+
+        System.out.println("-----制作花生豆浆-------");
+        SoyaMilk peanutSoyaMilk = new PeanutSoyaMilk();
+        peanutSoyaMilk.make();
+    }
+}
---
+++ src/main/java/TemplatePattern/RedBeanSoyaMilk.java
@@ -0,0 +1,13 @@
+package TemplatePattern;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-30 15:14
+ */
+public class RedBeanSoyaMilk extends SoyaMilk{
+    @Override
+    void addCondiments() {
+        System.out.println("加入上好的红豆");
+    }
+}
