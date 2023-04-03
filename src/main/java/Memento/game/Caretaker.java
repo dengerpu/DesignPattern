4184a224e844abeac8f0c0a2a41598f446426fa4
dep <1649743146@qq.com>
2023-04-03 16:46:46 +0800

    备忘录模式应用

---
+++ src/main/java/Memento/game/Caretaker.java
@@ -0,0 +1,28 @@
+package Memento.game;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-04-03 16:38
+ */
+public class Caretaker {
+
+    // 如果只保存一次状态
+    private Memento memento;
+    // 对GameRole保存多次状态
+    private ArrayList<Memento> mementos;
+
+    // 对多个游戏角色保存多个状态
+    private HashMap<String, ArrayList<Memento>> rolesMementos;
+
+    public Memento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+}
---
+++ src/main/java/Memento/game/Memento.java
@@ -0,0 +1,36 @@
+package Memento.game;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-04-03 16:34
+ */
+// 备忘录对象
+public class Memento {
+
+    // 攻击力
+    private int vit;
+    // 防御力
+    private int def;
+
+    public Memento(int vit, int def) {
+        this.vit = vit;
+        this.def = def;
+    }
+
+    public int getVit() {
+        return vit;
+    }
+
+    public void setVit(int vit) {
+        this.vit = vit;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+}
